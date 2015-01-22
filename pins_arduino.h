4c801ef32ac5945c9058d56913dde78124bb80e0
remi <remi.pincent@inria.fr>
2015-01-22 19:01:32 +0100

    Merge redbear arduino core. Clean template

--- pins_arduino.h
+++ pins_arduino.h
@@ -42,44 +42,6 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _UART_CLASS_
-#define _UART_CLASS_
+// API compatibility
+#include "variant.h"
 
-#include "HardwareSerial.h"
-#include "RingBuffer.h"
-//#include "variant.h"
-
-typedef void Uart;
-
-class UARTClass : public HardwareSerial
-{
-  protected:
-    RingBuffer *_rx_buffer, *_tx_buffer ;
-    volatile bool transmitting;
-    void tx( void );
-
-  public:
-    UARTClass( RingBuffer* pRx_buffer, RingBuffer* pTx_Buffer ) ;
-
-    void begin( const uint32_t dwBaudRate ) ;
-	  void begin( const uint32_t dwBaudRate, uint8_t rx_pin, uint8_t tx_pin ) ;
-    void end( void ) ;
-    int available( void ) ;
-    int peek( void ) ;
-    int read( void ) ;
-    void flush( void ) ;
-    size_t write( const uint8_t c ) ;
-
-    void IrqHandler( void ) ;
-
-#if defined __GNUC__ /* GCC CS3 */
-    using Print::write ; // pull in write(str) and write(buf, size) from Print
-#elif defined __ICCARM__ /* IAR Ewarm 5.41+ */
-//    virtual void write( const char *str ) ;
-//    virtual void write( const uint8_t *buffer, size_t size ) ;
-#endif
-
-    operator bool() { return true; }; // UART always active
-};
-
-#endif // _UART_CLASS_
